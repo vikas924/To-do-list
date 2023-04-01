d7130bdbb8d5121f293d8a7b1766b142beb1fd2f
vikas924 <obroy902@gmail.com>
2023-04-01 19:49:18 +0530

    update status

--- src/modules/status.js
+++ src/modules/status.js
@@ -7,22 +7,24 @@ export default function status() {
   const hide = document.querySelectorAll('.hide');
   const icon = document.querySelectorAll('.jvicon');
   let value = false;
-  if (this.checked === true) {
-    value = true;
-    tasks[index].completed = value;
-    input[index].style.textDecoration = 'line-through';
-    input[index].style.color = 'rgba(139, 134, 134, 0.8)';
-    input[index].disabled = true;
-    icon[index].style.display = 'none';
-    hide[index].style.display = 'block';
-    localStorage.setItem('array', JSON.stringify(tasks));
-  } else {
-    tasks[index].completed = value;
-    input[index].style.textDecoration = 'none';
-    input[index].style.color = 'inherit';
-    input[index].disabled = false;
-    icon[index].style.display = 'block';
-    hide[index].style.display = 'none';
-    localStorage.setItem('array', JSON.stringify(tasks));
-  }
+  setTimeout(() => {
+    if (this.checked === true) {
+      value = true;
+      tasks[index].completed = value;
+      input[index].style.textDecoration = 'line-through';
+      input[index].style.color = 'rgba(139, 134, 134, 0.8)';
+      input[index].disabled = true;
+      icon[index].style.display = 'none';
+      hide[index].style.display = 'block';
+      localStorage.setItem('array', JSON.stringify(tasks));
+    } else {
+      tasks[index].completed = value;
+      input[index].style.textDecoration = 'none';
+      input[index].style.color = 'inherit';
+      input[index].disabled = false;
+      icon[index].style.display = 'block';
+      hide[index].style.display = 'none';
+      localStorage.setItem('array', JSON.stringify(tasks));
+    }
+  }, 100);
 }
